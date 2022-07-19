0ff7c34f49aed573156849c25566b1430def121a
LLLLxr <1261620993@qq.com>
2022-07-19 21:22:24 +0800

    初始化

---
+++ src/main/java/Pos/Item.java
@@ -0,0 +1,13 @@
+package Pos;
+
+public class Item {
+    String itemName;
+    String itemNumber;
+    Integer price;
+
+    public Item(String itemName, String itemNumber, Integer price) {
+        this.itemName = itemName;
+        this.itemNumber = itemNumber;
+        this.price = price;
+    }
+}
---
+++ src/main/java/Pos/Pos.java
@@ -0,0 +1,40 @@
+package Pos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Pos {
+    public List<List<String>> generateReceipt(HashMap<String,Item> database,List<String> orders){
+        List<List<String>> receipts = new ArrayList<>();
+
+        return receipts;
+    }
+
+    public List<String> generateOrders(List<String> orders){
+        List<String> receipts = null;
+        return receipts;
+    }
+
+    public String generateTotalPrice(HashMap<String,Item> database,List<String> orders){
+        String totalPrice = null;
+
+        return totalPrice;
+    }
+
+    public List<String> generateOtherLine(){
+        List<String> otherLines = null;
+
+        return otherLines;
+    }
+
+    public String generatereceipt(List<String> orders){
+        String receipt = null;
+        return receipt;
+    }
+
+    public int caculateOrderPrice(HashMap<String,Item> database,String order){
+        int price = 0;
+        return price;
+    }
+}
